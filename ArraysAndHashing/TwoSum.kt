42ef67d577152d7cc7ba858dd31f58d4baa2db26
subbukathir <subbaiah@lovelocal.in>
2026-03-31 13:14:36 +0530

    Added ValidAnagram.kt TwoSum.kt to the streak

---
+++ ArraysAndHashing/TwoSum.kt
@@ -0,0 +1,65 @@
+/**
+ * Two Sum
+ * Easy
+ * Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+ *
+ * You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+ *
+ * Return the answer with the smaller index first.
+ *
+ * Example 1:
+ *
+ * Input:
+ * nums = [3,4,5,6], target = 7
+ *
+ * Output: [0,1]
+ * Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+ *
+ * Example 2:
+ *
+ * Input: nums = [4,5,6], target = 10
+ *
+ * Output: [0,2]
+ * Example 3:
+ *
+ * Input: nums = [5,5], target = 10
+ *
+ * Output: [0,1]
+ * Constraints:
+ *
+ * 2 <= nums.length <= 1000
+ * -10,000,000 <= nums[i] <= 10,000,000
+ * -10,000,000 <= target <= 10,000,000
+ * Only one valid answer exists.
+ */
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val numMap = mutableMapOf<Int, Int>()
+
+    for(i in nums.indices){
+        val complement = target - nums[i]
+        if(numMap.containsKey(complement)){
+            return intArrayOf(numMap[complement]!!, i)
+        }else{
+            numMap[nums[i]] = i
+        }
+    }
+    return intArrayOf()
+}
+
+fun main(){
+    val nums1 = intArrayOf(3,4,5,6)
+    val target1 = 7
+    val result1 = twoSum(nums1, target1)
+    println("Example 1: Input: ${nums1.contentToString()}, target: $target1, Output: ${result1.contentToString()}")
+
+    val nums2 = intArrayOf(4,5,6)
+    val target2 = 10
+    val result2 = twoSum(nums2, target2)
+    println("Example 2: Input: ${nums2.contentToString()}, target: $target2, Output: ${result2.contentToString()}")
+
+    val nums3 = intArrayOf(5,5)
+    val target3 = 10
+    val result3 = twoSum(nums3, target3)
+    println("Example 1: Input: ${nums3.contentToString()}, target: $target3, Output: ${result3.contentToString()}")
+}
---
+++ ArraysAndHashing/ValidAnagram.kt
@@ -0,0 +1,63 @@
+/**
+ * Valid Anagram
+ * Easy
+ * Given two strings s and t, return true if the two strings are anagrams of each other, otherwise
+ * return false.
+ *
+ * An anagram is a string that contains the exact same characters as another string,
+ * but the order of the characters can be different.
+ *
+ * Example 1:
+ *
+ * Input: s = "racecar", t = "carrace"
+ *
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "jar", t = "jam"
+ *
+ * Output: false
+ * Constraints:
+ *
+ * s and t consist of lowercase English letters.
+ */
+fun isAnagram(s: String, t: String): Boolean {
+    if (s.length != t.length) return false
+
+    val strMap = mutableMapOf<Char, Int>()
+    for (char in s) {
+        strMap[char] = strMap.getOrDefault(char, 0) + 1
+    }
+    for (char in t) {
+        val count = strMap.getOrDefault(char, 0)
+        if (count == 0) {
+            return false
+        }
+        strMap[char] = count - 1
+    }
+    return true
+}
+
+fun isAnagramAlternate(s: String, t: String): Boolean {
+    if (s.length != t.length) return false
+    val freqArr = IntArray(26)
+    for(i in s.indices){
+        freqArr[s[i] - 'a']++
+        freqArr[t[i] - 'a']--
+    }
+    return freqArr.all { it == 0 }
+}
+
+fun main() {
+    val s1 = "racecar"
+    val t1 = "carrace"
+
+    val result1 = isAnagramAlternate(s1, t1)
+    println("Example 1: Input: s = \"$s1\", t = \"$t1\", Output: $result1")
+
+    val s2 = "jar"
+    val t2 = "jam"
+
+    val result2 = isAnagramAlternate(s2, t2)
+    println("Example 2: Input: s = \"$s2\", t = \"$t2\", Output: $result2")
+}
